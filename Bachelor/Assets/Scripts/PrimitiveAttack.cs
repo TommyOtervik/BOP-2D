3015062e3dc41852e33c7640b2621173bed7cbe4
Tommy Otervik <tommyeotervik@gmail.com>
2021-02-26 20:19:00 +0100

    New stuff
    d

---
+++ Bachelor/Assets/Scripts/PrimitiveAttack.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrimitiveAttack : MonoBehaviour
+{
+    public Transform attackPoint;
+
+    public GameObject bulletPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Attack1();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Attack1();
+    }
+
+    void Attack1()
+    {
+        GameObject tempBullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity, null);
+        tempBullet.GetComponent<PrimitiveObjectMove>().Init(Vector2.left);
+        
+        tempBullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity, null);
+        tempBullet.GetComponent<PrimitiveObjectMove>().Init(Vector2.up);
+        
+        tempBullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity, null);
+        tempBullet.GetComponent<PrimitiveObjectMove>().Init(Vector2.right);
+        
+        tempBullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity, null);
+        tempBullet.GetComponent<PrimitiveObjectMove>().Init(Vector2.down);
+        
+        
+        
+    }
+
+    void Attack2()
+    {
+        
+    }
+}
---
+++ Bachelor/Assets/Scripts/PrimitiveObjectMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrimitiveObjectMove : MonoBehaviour
+{
+    private Vector2 direction;
+    private float speed = 10.0f;
+    // Use this for initialization
+
+    public void Init(Vector2 direction)
+    {
+        this.direction = direction;
+    }
+
+    void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+    
+    void OnTriggerEnter2D (Collider2D target) {
+        Destroy(gameObject);
+    }
+    
+
+}
+
+
+
