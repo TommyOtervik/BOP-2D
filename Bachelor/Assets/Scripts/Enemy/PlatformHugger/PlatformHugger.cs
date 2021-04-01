d8d58aa7befde84d9c52eba50ffdf1acc1cd1bc5
Tommy Otervik <tommyeotervik@gmail.com>
2021-04-01 23:35:31 +0200

    Platform Hugger
    // Viktig info, alle 4 posisjoner må ha en -90 Z rotasjon.
            // Sett i tillegg startposisjon til objektet sånn at han løper til høyre mot punkt 1 i positions array. Visst ikke blir det rotasjonskrøll.
            // 4    ----------------     1
            // 3    ----------------     2

--- Bachelor/Assets/Scripts/Enemy/PlatformHugger/PlatformHugger.cs
+++ Bachelor/Assets/Scripts/Enemy/PlatformHugger/PlatformHugger.cs
@@ -1,4 +1,9 @@
-﻿using UnityEngine;
+﻿using System.Numerics;
+using UnityEngine;
+
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
+
 public class PlatformHugger : MonoBehaviour
     {
         [SerializeField] private Transform[] positions;
@@ -9,6 +14,9 @@ public class PlatformHugger : MonoBehaviour
         [SerializeField] private int collisionDamageAmount = 10;
         private const string PLAYER_NAME = "Player";
 
+        
+        // Rotasjonstesting
+        private float distanceToPoint; // This will store the remaining distance between player and _nextPos.transform
         void Start()
         {
             _nextPos = positions[0];
@@ -23,6 +31,10 @@ public class PlatformHugger : MonoBehaviour
         {
             if (transform.position == _nextPos.position)
             {
+                // Rotasjonstest
+                Rotate();
+                
+                // Originalet
                 _nextPosIndex++;
                 if (_nextPosIndex >= positions.Length)
                 {
@@ -30,6 +42,7 @@ public class PlatformHugger : MonoBehaviour
                 }
 
                 _nextPos = positions[_nextPosIndex];
+                
             }
             else
             {
@@ -37,7 +50,8 @@ public class PlatformHugger : MonoBehaviour
             
             }
         }
-        /*
+        
+        
         private void OnTriggerEnter2D(Collider2D collision)
         {
         
@@ -47,5 +61,16 @@ public class PlatformHugger : MonoBehaviour
             }
 
         }
-        */
+        // Viktig info, alle 4 posisjoner må ha en -90 Z rotasjon.
+        // Sett i tillegg startposisjon til objektet sånn at han løper til høyre mot punkt 1 i positions array. Visst ikke blir det rotasjonskrøll.
+        // 4    ----------------     1
+        // 3    ----------------     2
+        
+        private void Rotate()
+        {
+            Vector3 currentRotation = transform.eulerAngles;
+            currentRotation.z += positions[_nextPosIndex].transform.eulerAngles.z;
+            transform.eulerAngles = currentRotation;
+        }
+
     }
