ba81620820f7ba22ca2e8b7a44165cf4fd48a818
Tommy Otervik <tommyeotervik@gmail.com>
2021-04-17 00:04:40 +0200

    d

--- Bachelor/Assets/Scripts/Pickups/Pickup.cs
+++ Bachelor/Assets/Scripts/Pickups/Pickup.cs
@@ -11,52 +11,118 @@ public abstract class Pickup : MonoBehaviour
     protected bool movingUp = true;
     protected float upperBoundYOffset = 0.6f;
     
+    //Raycast stuff
+    public LayerMask groundLayer;
+    public bool drawDebugRaycasts = true;
+    public bool hitGround;
+    public BoxCollider2D collider;
+    
     // Start is called before the first frame update
     void Start()
     {
         startPosition = transform.position;
         upperBound = new Vector2(startPosition.x, startPosition.y + upperBoundYOffset);
         lowerBound = startPosition;
+        collider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        PhysicsCheck();
         Move();
     }
     
 
     void Move()
     {
-        if (movingUp)
+        if (!hitGround)
         {
-            if (transform.position.y >= upperBound.y)
+            //transform.position = Vector2.MoveTowards(transform.position, Vector2.right, speed * Time.deltaTime);
+            transform.Translate(Vector3.down * speed * Time.deltaTime);
+        }
+
+        else
+        {
+            if (movingUp)
             {
-                transform.position = Vector2.MoveTowards(transform.position, lowerBound, speed * Time.deltaTime);
-                movingUp = false;
-            }
-            else
-            {
-                transform.position = Vector2.MoveTowards(transform.position, upperBound, speed * Time.deltaTime);
-            }
+                if (transform.position.y >= upperBound.y)
+                {
+                    transform.position = Vector2.MoveTowards(transform.position, lowerBound, speed * Time.deltaTime);
+                    movingUp = false;
+                }
+                else
+                {
+                    transform.position = Vector2.MoveTowards(transform.position, upperBound, speed * Time.deltaTime);
+                }
 
             
-        }
+            }
         
-        else if (!movingUp)
-        {
-            if (transform.position.y <= lowerBound.y)
+            else if (!movingUp)
             {
-                transform.position = Vector2.MoveTowards(transform.position, upperBound, speed * Time.deltaTime);
-                movingUp = true;
-            }
-            else
-            {
-                transform.position = Vector2.MoveTowards(transform.position, lowerBound, speed * Time.deltaTime);
-            }
+                if (transform.position.y <= lowerBound.y)
+                {
+                    transform.position = Vector2.MoveTowards(transform.position, upperBound, speed * Time.deltaTime);
+                    movingUp = true;
+                }
+                else
+                {
+                    transform.position = Vector2.MoveTowards(transform.position, lowerBound, speed * Time.deltaTime);
+                }
 
             
+            }
         }
 
+        
+
+    }
+
+    void PhysicsCheck()
+    {
+        if (hitGround)
+        {
+            return;
+        }
+
+        RaycastHit2D groundCheck = Raycast(new Vector2(0f, collider.size.y), Vector2.down, 0.3f);
+        if (groundCheck)
+        {
+            // Overstyrer upperbound og lowerbound i det jeg treffer bakken for å unngå at pickup går mot originalposisjon
+            lowerBound = transform.position;
+            upperBound = new Vector2(lowerBound.x, lowerBound.y + upperBoundYOffset);
+            hitGround = true;
+        }
+    }
+
+    //These two Raycast methods wrap the Physics2D.Raycast() and provide some extra
+    //functionality
+    RaycastHit2D Raycast(Vector2 offset, Vector2 rayDirection, float length)
+    {
+        //Call the overloaded Raycast() method using the ground layermask and return 
+        //the results
+        return Raycast(offset, rayDirection, length, groundLayer);
+    }
+
+    RaycastHit2D Raycast(Vector2 offset, Vector2 rayDirection, float length, LayerMask mask)
+    {
+        //Record the player's position
+        Vector2 pos = transform.position;
+
+        //Send out the desired raycast and record the result
+        RaycastHit2D hit = Physics2D.Raycast(pos + offset, rayDirection, length, mask);
+
+        //If we want to show debug raycasts in the scene...
+        if (drawDebugRaycasts)
+        {
+            //...determine the color based on if the raycast hit...
+            Color color = hit ? Color.red : Color.green;
+            //...and draw the ray in the scene view
+            Debug.DrawRay(pos + offset, rayDirection * length, color);
+        }
+
+        //Return the results of the raycast
+        return hit;
     }
 }
