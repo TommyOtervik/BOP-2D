8cfee3f6cf81bf8cd11ee23d6e75b48876ba838f
Tommy Otervik <tommyeotervik@gmail.com>
2021-04-26 17:58:26 +0200

    Skully and stuff

---
+++ Bachelor/Assets/Scripts/Enemy/Skully/SkullBossSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkullBossSpawner : MonoBehaviour
+{
+    [SerializeField] public Transform spawnPointLeft;
+    [SerializeField] public Transform spawnPointRight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
