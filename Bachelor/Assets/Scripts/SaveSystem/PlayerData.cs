cc8320a516d09307629517fe9ac921bdbe5e9bef
Olaiski <kungbinders@gmail.com>
2021-05-05 19:34:25 +0200

    Update PlayerData.cs

--- Bachelor/Assets/Scripts/SaveSystem/PlayerData.cs
+++ Bachelor/Assets/Scripts/SaveSystem/PlayerData.cs
@@ -12,15 +12,12 @@ using UnityEngine;
 public class PlayerData
 {
     private int currentHealth;
-    private float[] position;
+
 
     public PlayerData(Player player)
     {
         currentHealth = player.GetCurrentHealth();
-        position = new float[3];
-        position[0] = player.transform.position.x;
-        position[1] = player.transform.position.y;
-        position[2] = player.transform.position.z;
+
     }
 
 
